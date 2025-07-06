872112b2d640b2ccb5d90bbf4ff4d2e034bfc512
Stan Griffiths <stantgriffiths@gmail.com>
2025-07-06 16:44:44 +0100

    Trigger test-pypi-upload workflow
    Minor comment update to trigger workflow after fixing platform-specific icon format tests

--- threepanewindows/__init__.py
+++ threepanewindows/__init__.py
@@ -71,4 +71,4 @@ __all__ = [
 ]
 
 # Note: FixedThreePaneWindow is the modern name, FixedThreePaneLayout is legacy
-# Trigger workflow to test import fixes
+# Trigger workflow to test platform-specific icon format fixes
